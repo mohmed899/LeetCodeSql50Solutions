eb23e402f808155cf147f22f0579fa74915e3326
mohmed899 <muhammedkhaled889@gmail.com>
2024-01-18 20:17:22 +0200

    620

---
+++ 620. Not Boring Movies.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Cinema (
+    id INT PRIMARY KEY,
+    movie VARCHAR(255),
+    description VARCHAR(255),
+    rating DECIMAL(3,1)
+);
+
+INSERT INTO Cinema (id, movie, description, rating) VALUES
+    (1, 'War', 'great 3D', 8.9),
+    (2, 'Science', 'fiction', 8.5),
+    (3, 'Irish', 'boring', 6.2),
+    (4, 'Ice song', 'Fantasy', 8.6),
+    (5, 'House card', 'Interesting', 9.1);
+
+	select * from Cinema where 
+id % 2 != 0 and description != 'boring'
+order by rating desc
